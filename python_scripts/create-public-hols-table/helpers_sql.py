965dcbf7637c34482683e252e6cf989cb4cf086c
jayruffellvw <152837969+jayruffellvw@users.noreply.github.com>
2024-10-31 14:37:33 +1300

    add arg for defining schema

--- python_scripts/create-public-hols-table/helpers_sql.py
+++ python_scripts/create-public-hols-table/helpers_sql.py
@@ -5,19 +5,34 @@ import requests
 import pandas as pd
 import numpy as np
 import logging
+from urllib.parse import quote
+import os
+
+username = os.getenv('TEST_DB_USERNAME')
+password = quote(os.getenv('TEST_DB_PASSWORD'))
+# username = quote('userForTestingSQLServerAuthentication')
+# password = quote('t3stingAuthentication!')
 
 # Note it was a pain in the butt to get the connection string right in SQL alchemy!!!
-def save_to_db(df, db_name, table_name):
+def save_to_db(df, db_name, schema_name, table_name):
     """save a pandasdf as table in my SQLDEV instance. Need to update connection string if using any other db instance. Returns True if successful and False otherwise."""
-
     connection_string_sqlalchemy = (
-        f"mssql+pyodbc://DESKTOP-JGNU8D2\\SQLDEV/{db_name}?"
+        f"mssql+pyodbc://{username}:{password}@DESKTOP-JGNU8D2\\SQLDEV/{db_name}?"
         "driver=ODBC+Driver+17+for+SQL+Server&"
-        "trusted_connection=yes&"
         "encrypt=yes&"
         "trustServerCertificate=yes"
     )
-    
+    # connection_string_sqlalchemy = connection_string_sqlalchemy.replace("'", "")
+    logging.info(f"connection string: {connection_string_sqlalchemy}")
+
+    # connection_string_sqlalchemy = (
+    #     f"mssql+pyodbc://DESKTOP-JGNU8D2\\SQLDEV/{db_name}?"
+    #     "driver=ODBC+Driver+17+for+SQL+Server&"
+    #     "trusted_connection=yes&"
+    #     "encrypt=yes&"
+    #     "trustServerCertificate=yes"
+    # )
+
     # -----------
     # # for troubleshooting – connection string that I know works.
     # -----------
@@ -30,7 +45,7 @@ def save_to_db(df, db_name, table_name):
     # )
     # logging.info(connection_string_sqlalchemy)
     # engine = create_engine(connection_string_sqlalchemy)
-    
+
     # # TEST CONNECTION IF DESIRED
     # query = "SELECT TOP 10 * FROM Sales.Dim_Date"
     # test_df = pd.read_sql(query, engine)
@@ -39,7 +54,7 @@ def save_to_db(df, db_name, table_name):
     engine = create_engine(connection_string_sqlalchemy)
 
     try:
-        df.to_sql(table_name, con=engine, if_exists="replace", index=False)
+        df.to_sql(table_name, con=engine, schema=schema_name, if_exists="replace", index=False)
         operation_successful = True
     except Exception as e:
         operation_successful = False
--- python_scripts/create-public-hols-table/run-main-from-cmd-line.ps1
+++ python_scripts/create-public-hols-table/run-main-from-cmd-line.ps1
@@ -1,2 +1,7 @@
 # cmd line equivalent of run.py
-python main.py 1960 2024 'staging' 'holidays'
+python main.py 1960 2024 'staging' 'python' 'holidays'
+
+
+# mssql+pyodbc://userForTestingSQLServerAuthentication:t3stingAuthentication%21@DESKTOP-JGNU8D2\SQLDEV/'staging'?driver=ODBC+Driver+17+for+SQL+Server&encrypt=yes&trustServerCertificate=yes
+
+# mssql+pyodbc://userForTestingSQLServerAuthentication:t3stingAuthentication%21@DESKTOP-JGNU8D2\SQLDEV/staging?driver=ODBC+Driver+17+for+SQL+Server&encrypt=yes&trustServerCertificate=yes
