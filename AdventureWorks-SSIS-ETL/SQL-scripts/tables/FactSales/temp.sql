7ea65174e047dbe772235d550d7c3819a23cd7e4
jayruffellvw <152837969+jayruffellvw@users.noreply.github.com>
2024-11-01 18:49:41 +1300

    update & rename SSIS ETL package - now works to load data into Fact table and recreate indexes.

--- AdventureWorks-SSIS-ETL/SQL-scripts/tables/FactSales/temp.sql
+++ AdventureWorks-SSIS-ETL/SQL-scripts/tables/FactSales/temp.sql
@@ -1,2 +1,7 @@
 use AdventureWorksDW_SSIS
-drop table Sales.Fact_SalesOrderDetail
+
+-- truncate table sales.Fact_SalesOrderDetail
+
+select count(*) from sales.Fact_SalesOrderDetail
+
+select top 1 * from sales.Fact_SalesOrderDetail
